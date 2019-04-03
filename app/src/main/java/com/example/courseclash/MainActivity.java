1d4343ab7992b9201f6fd03d7e618e177e4d0cdc
Toni <t7koto00@students.oamk.fi>
2019-04-03 11:06:08 +0300

    AddRecipe
    AddRecipe activity first commit

--- app/src/main/java/com/example/courseclash/MainActivity.java
+++ app/src/main/java/com/example/courseclash/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.courseclash;
 
 import android.content.Intent;
+import android.location.Address;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,7 +13,7 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, RecipeListView.class);
+        Intent intent = new Intent(this, AddRecipe.class);
         startActivity(intent);
 
     }
