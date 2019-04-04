c2270436d8c22b8bfe707fad0d212253eb57841e
Ossi Oppilas <t7ryan00@students.oamk.fi>
2019-04-04 11:03:44 +0300

    Buttons

--- app/src/main/java/com/example/courseclash/MainActivity.java
+++ app/src/main/java/com/example/courseclash/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.courseclash;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button ingredientBtn;
+    Button createBtn;
+    EditText goToSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ingredientBtn = findViewById(R.id.ingredientBtn);
+        createBtn = findViewById(R.id.createBtn);
+        goToSearch = findViewById(R.id.goToSearch);
+
     }
 }
