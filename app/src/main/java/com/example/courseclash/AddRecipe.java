4bd17d4a76848133e4aa6ff52d515e38ed9289f7
Toni <t7koto00@students.oamk.fi>
2019-04-04 13:26:46 +0300

    Addrecipe
    Kuvan haku galleriasta ominaisuus

--- app/src/main/java/com/example/courseclash/AddRecipe.java
+++ app/src/main/java/com/example/courseclash/AddRecipe.java
@@ -1,16 +1,22 @@
 package com.example.courseclash;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.io.IOException;
 
 public class AddRecipe extends AppCompatActivity implements View.OnClickListener {
 
@@ -24,6 +30,9 @@ public class AddRecipe extends AppCompatActivity implements View.OnClickListener
     public Button addButton = null;
     Recipe recipe = new Recipe();
     FirebaseFirestore db = null;
+    public ImageView imageView = null;
+    private final int PICK_IMAGE_REQUEST = 71;
+    private Uri filePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +50,7 @@ public class AddRecipe extends AppCompatActivity implements View.OnClickListener
         galleryButton.setOnClickListener(this);
         cameraButton.setOnClickListener(this);
         addButton.setOnClickListener(this);
-
+        imageView = findViewById(R.id.imageViewAdd);
         db = FirebaseFirestore.getInstance();
 
     }
@@ -53,6 +62,10 @@ public class AddRecipe extends AppCompatActivity implements View.OnClickListener
         }
         else if (view == galleryButton){
         // kuvan valinta galleriasta
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
         }
         else if (view == addButton){
 
@@ -84,6 +97,24 @@ public class AddRecipe extends AppCompatActivity implements View.OnClickListener
         }
     });
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
