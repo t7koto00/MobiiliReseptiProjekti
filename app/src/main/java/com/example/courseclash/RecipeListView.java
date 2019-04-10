402997127b034a058a505269f47fe46d0909e4fb
Ossi Oppilas <t7ryan00@students.oamk.fi>
2019-04-10 12:32:46 +0300

    Toolbar and burger menu update
    Toolbar and burger menu shown in every view

--- app/src/main/java/com/example/courseclash/RecipeListView.java
+++ app/src/main/java/com/example/courseclash/RecipeListView.java
@@ -1,13 +1,16 @@
 package com.example.courseclash;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ListView;
 import android.support.v7.widget.SearchView;
 
@@ -19,7 +22,7 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 
-public class RecipeListView extends AppCompatActivity {
+public class RecipeListView extends BaseActivity {
 
     public ArrayList<Recipe> recipeList = new ArrayList<>();
     ListView listView = null;
@@ -28,9 +31,13 @@ public class RecipeListView extends AppCompatActivity {
     RecipeViewAdapter rAdapter = null;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_recipe_view);
+        //setContentView(R.layout.activity_recipe_view);
+        LayoutInflater inflater = (LayoutInflater) getApplicationContext()
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View contentView = inflater.inflate(R.layout.activity_recipe_view, null, false);
+        drawer.addView(contentView, 0);
 
         listView = findViewById(R.id.recipe_listview);
     /*
